93d66e57231c7dafca3c913a678d6cd44bd59310
Raul Holanda <raul.dev@edu.unifor.br>
2024-04-29 10:34:08 -0300

    commitando para demonstrar a criacao de uma referencia 'Cliente' diretamente acessando 'contaDaMarcela.titular'

--- bytebankComposto/FundamentosOO/TestaContaSemCliente.java
+++ bytebankComposto/FundamentosOO/TestaContaSemCliente.java
@@ -5,9 +5,10 @@ public class TestaContaSemCliente {
         Conta contaDaMarcela = new Conta();
         System.out.println(contaDaMarcela.saldo);
 
-        System.out.println(contaDaMarcela.titular); // null
-        contaDaMarcela.titular.nome = "Marcela";
-        System.out.println(contaDaMarcela.titular.nome); // EXCECAO: `NullPointerException`
-        // O '0' de quando a "variavel" do tipo REFERENCIA (OBJETO) e chamado null; quer dizer "referencia pra lugar nenhum"
+        contaDaMarcela.titular = new Cliente(); //AQUI: momento onde ja instancio a referencia para o atributo `titular` da contaDaMarcela
+        System.out.println(contaDaMarcela.titular); //bytebankComposto.FundamentosOO.Cliente@5305068a
+        contaDaMarcela.titular.nome = "Marcela"; //Marcela
+        System.out.println(contaDaMarcela.titular.nome); 
+       
     }
 }
