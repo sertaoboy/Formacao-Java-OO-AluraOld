3bc50c62dc8247cd686c413a7e539654572c74ea
Raul Holanda <raul.dev@edu.unifor.br>
2024-05-02 12:57:49 -0300

    introduzindo atributos estaticos; implementacoes; exemplos

--- bytebankComposto/bytebankEncapsulado/Conta.java
+++ bytebankComposto/bytebankEncapsulado/Conta.java
@@ -5,14 +5,23 @@ public class Conta {
     private int agencia;                            
     private int numero;
     private Cliente titular;
+    private static int total; //INTRODUCAO A PALAVRA CHAVE `static`: Paulo deu o exemplo de como lemos os atributos de um objeto/classe; Neste caso, todo OBJETO do tipo Conta, TEM atributos `saldo`, `titular`, `agencia`, `numero`
+    //Para dizer que UM ATRIBUTO pertence a CLASSE (nao necessariamente um objeto como o comentario de cima), utiliza-se `static`, que se le: toda CLASSE Conta tem um atributo `titular` (diferente de dizer que: todo OBJETO da CLASSE Conta tem um atributo `total`)
+    //"O static, tem uma cara de variavel global. Porem global aos objetos."
+    //Agora, garantimos que TODO OBJETO do tipo Conta tem acesso a um UNICO `total`!!!
+
+    //O TOTAL NAO E MAIS EXCLUSIVO DE UMA INSTANCIA, UM OBJETO, E SIM UM ATRIBUTO COMPARTILHADO ENTRE ELAS/ELES: `static`
     
 
     public Conta(int agencia, int numero){ 
+        Conta.total++; // usar o this neste caso, nao e viavel, pois o this se referencia ao OBJETO que foi instanciado. A partir do momento que temos um atributo estatico, dizemos que nao e mais um objeto que possui este atributo e sim a CLASSE
         if(agencia <= 0 || numero <= 0) {
             System.out.println("Agencia e Numero nao se pode ter valor 0 ou negativo");
         }else{
+            System.out.println("O total de contas instanciadas e: "+Conta.total);
             this.agencia=agencia;
             this.numero=numero;
+            System.out.println("Criando uma conta com o numero: "+this.numero+", e agencia: "+this.agencia+".");
         }
     }//Esse construtor implica na razao da existencia do metodos setAgencia() e setNumero()?? DEPENDE DA ESPECIFICACAO; bom para reflexao; "Atributo imutavel caso sim, pois depois que ele foi criado, nunca mais sera alterado.", Paulo Silveira
     
@@ -86,4 +95,14 @@ public class Conta {
     public void setTitular(Cliente titular) {
         this.titular = titular;
     }
+
+    
+    public static int  getTotal(){ //getter de um atributo estatico
+        return Conta.total;
+        //System.out.println(this.saldo); explicacao abaixo.
+    }// reparar no `static` novamente: diferente dos outros metodos get e set, este em particular nao e restrito a um objeto/instancia, e sim a classe Conta, onde os objetos compartilham de um unico atributo especificado (static)
+    //EXPLICACAO: dentro de um metodo estatico nao tem como existir `this`. Porque quem invoca o metodo nao e uma referencia ou instancia/objeto e sim a propria CLASSE
+
+    //ATRIBUTOS DE INSTANCIA != ATRIBUTOS DE CLASSE
+
 }
--- bytebankComposto/bytebankEncapsulado/TestaValores.java
+++ bytebankComposto/bytebankEncapsulado/TestaValores.java
@@ -14,6 +14,10 @@ public class TestaValores {
         Conta conta = new Conta(1337, 24226);//obrigamos que, ao criar uma nova Conta, passe a `agencia` e o `numero`
         System.out.println(conta.getAgencia());// retorna 0; por conta do valor default ao se instanciar um objeto na referencia `conta`
         //impressao que o atributo nasce com um problema; "nasce em um estado inconsistente"
+
+        Conta conta2 = new Conta(1337,16549);
+
+        System.out.println(Conta.getTotal());
         
 
     
