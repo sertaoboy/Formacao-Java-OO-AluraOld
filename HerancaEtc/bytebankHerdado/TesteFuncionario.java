b0c6a1148263b19b2f076cc9cb54f8745cfc32c6
Raul Holanda <raul.dev@edu.unifor.br>
2024-05-14 12:35:18 -0300

    ...

--- HerancaEtc/bytebankHerdado/TesteFuncionario.java
+++ HerancaEtc/bytebankHerdado/TesteFuncionario.java
@@ -2,13 +2,14 @@ package HerancaEtc.bytebankHerdado;
 
 public class TesteFuncionario {
     public static void main(String[] args) {
-        Funcionario nico = new Funcionario(); //curiosidade: se nao for definido o construtor, o compialdor popula com os valores padores
-        nico.setNome("Nico Steppat");
-        nico.setCpf("2223332-9");
-        nico.setSalario(2600.80);
+        //Funcionario nico = new Funcionario(); //curiosidade: se nao for definido o construtor, o compialdor popula com os valores padores
+        //nico.setNome("Nico Steppat");
+        //nico.setCpf("2223332-9");
+        //nico.setSalario(2600.80);
 
-        System.out.println(nico.getNome());
-        System.out.println(nico.getBonificacao());
+        //System.out.println(nico.getNome());
+       // System.out.println(nico.getBonificacao());
+       
 
     }
 }
